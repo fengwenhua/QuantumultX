df51e12ad70fcf7193be49bf53076af407bf0006
冯文华 <26518808+fengwenhua@users.noreply.github.com>
2023-10-16 05:06:58 -0500

    Create Notability1.js

---
+++ Notability1.js
@@ -0,0 +1,46 @@
+/******************************
+
+脚本功能：Notability + 解锁订阅
+软件版本：14.3
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！
+
+
+*******************************
+
+[rewrite_local]
+
+
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Notability.js
+
+[mitm] 
+
+hostname = notability.com
+
+*******************************/
+
+var obj = JSON.parse($response.body);
+var modifiedStatus = "HTTP/1.1 200 OK";
+obj = {
+  "data": {
+    "processAppleReceipt": {
+      "__typename": "SubscriptionResult",
+      "error": 0,
+      "subscription": {
+        "__typename": "AppStoreSubscription",
+        "status": "active",
+        "originalPurchaseDate": "2020-09-28T09:09:09.000Z",
+        "originalTransactionId": "100109876543210",
+        "expirationDate": "2999-09-28T09:09:09.000Z",
+        "productId": "com.yqc.premium_subscription",
+        "tier": "premium",
+        "refundedDate": null,
+        "refundedReason": null,
+        "user": null
+      }
+    }
+  }
+};
+$done({
+  "status": modifiedStatus,
+  "body": JSON.stringify(obj)
+});
