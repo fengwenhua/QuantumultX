6a32e7fada1e112de868b65e9099fd111133f138
冯文华 <26518808+fengwenhua@users.noreply.github.com>
2024-03-13 15:34:17 +0800

    Create baguwen.js

---
+++ baguwen.js
@@ -0,0 +1,26 @@
+/*
+ *
+ *
+脚本功能：校招八股文无限阅读
+脚本作者：江南小虫虫
+更新时间：2024-3-13
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+[rewrite_local]
+# > 校招八股文无限阅读
+^https:\/\/study-api\.playoffer\.cn:443\/wxUser\/payOneQuestion url script-response-body https://raw.githubusercontent.com/fengwenhua/QuantumultX/main/GXNOVA.js
+^https:\/\/study-api\.playoffer\.cn:443\/wxUser\/payOneQuestion url response-body code":\d\d\d response-body code":200
+[mitm] 
+hostname = study-api.playoffer.cn
+*
+*
+*/
+
+
+var body = $response.body;
+var hack = /"code":\d\d\d/g;
+body = body.replace(hack,'"code":200');
+$done({
+    body
+});
